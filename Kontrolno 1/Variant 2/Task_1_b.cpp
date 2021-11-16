136a346ba263b01fda9d6132d5fb5c66aaa3fc47
Aleksandar-Karatov <32852359+Aleksandar-Karatov@users.noreply.github.com>
2021-11-16 11:33:44 +0200

    Variant 2

---
+++ Kontrolno 1/Variant 2/Task_1_b.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double x, y;
+    bool check = false;
+    cin >> x >> y;
+    if (x <= 2 && x >= -2 && y <= 2 && y >= -2) {
+        check = true;
+    }
+    if (y <= 0 && x * x + (y + 2) * (y + 2) < 4) {
+        check = false;
+    }
+    cout << (check ? "Yes" : "No");
+    return 0;
+}
---
+++ Kontrolno 1/Variant 2/Task_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	if (n > 2 && n < 26) {
+		for (int i = 0; i < 6 + 2 * n; i++) {
+			if (i < 5 || i > n * 2) {
+				cout << "*";
+			}
+			else cout << " ";
+		}
+		cout << endl;
+		for (int j = 0; j < n - 2; j++) {
+			for (int j1 = 0; j1 < 6 + 2 * n; j1++) {
+				if (j1 == j + 1 || j1 == j + 5 ||
+					j1 == 4 + 2 * n - j || j1 == 2 * n - j) {
+					cout << "*";
+				}
+				else cout << " ";
+			}
+			cout << endl;
+		}
+		for (int k = 0; k < 6 + 2 * n; k++) {
+			if (k > n - 2 && k < n + 7) {
+				cout << "*";
+			}
+			else cout << " ";
+		}
+	}
+	else cout << "Wrong number";
+	return 0;
+}
